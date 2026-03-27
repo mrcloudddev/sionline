34f935eba06f4842a6de9d273b156c3e9f10446b
mrclouddev <166690757+mrcloudddev@users.noreply.github.com>
2026-03-27 16:08:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // GANTI LINK DI BAWAH INI DENGAN LINK DEPLOYMENT APPS SCRIPT TERBARU
-const BASE_URL = "https://script.google.com/macros/s/AKfycbxqgphHpQvmF80rpTdOi5oRIqJz9I-gy5iQNZRiGUs_8uKqR0jozyOC7THD1Vw6iwA/exec";
+const BASE_URL = "https://script.google.com/macros/s/AKfycbyUe5E5hkX8FY3GM_yoTef458yu7iUsRJKRYGa6PUIJ-v6cVmu_ZFnqlARFWHGH0Jrl/exec";
 
 let dataSiswaAktif = {};
 let timerInterval;
