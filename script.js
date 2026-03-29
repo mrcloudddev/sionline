d34f5996f4614f99571fbcd3e05b13a060c7c44c
mrclouddev <166690757+mrcloudddev@users.noreply.github.com>
2026-03-29 14:14:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  * CBT SMK - CLIENT FINAL FIX TOTAL (SOAL PASTI MUNCUL)
  */
 
-const BASE_URL = "https://script.google.com/macros/s/AKfycbw-kYl3iXufprFMAnIq0kvFkDovAUHQ-jgB-__7yjOKo-mHq2CHT_meK9YTS27i4ys4/exec";
+const BASE_URL = "https://script.google.com/macros/s/AKfycbwKQdrV04IANysd08p-0XdTTKWTJaGgAOIjweU9_eZv6Nk9zW0i9MarHyNUMaiLM4Vd/exec";
 
 let dataSiswaAktif = {};
 let timerInterval;
